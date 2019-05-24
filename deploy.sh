667abdc17ee4e381d58de9554d7a1840b596e2c5
sudoist <mail@jcos.io>
2019-05-25 02:54:13 +0800

    Added Travis CI for automated build
    Refs:
    - https://monicalent.com/blog/2017/12/21/deploy-static-sites-digital-ocean-travis/
    - https://kjaer.io/travis/
    - https://travis-ci.community/t/solved-openssl-decrypt-yields-bad-decrypt-for-a-single-file/2886

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -xe
+
+if [ $TRAVIS_BRANCH == 'master' ] ; then
+  eval "$(ssh-agent -s)"
+  ssh-add ~/.ssh/id_rsa
+
+  cd public
+  git init
+
+  git remote add deploy "travis@johncosio.com:/home/web/app/prod/hugo-jcos.io"
+  git config user.name "Travis CI"
+  git config user.email "mail@jcos.io"
+
+  git add .
+  git commit -m "Deployed with Travis"
+  git push deploy master
+else
+  echo "Not deploying, since this branch isn't master."
+fi
